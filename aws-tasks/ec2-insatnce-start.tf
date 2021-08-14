995aa01b514c183ad61c91fa70c707259142411d
manojreddy <ymrdevops@gmail.com>
2021-08-14 07:10:32 +0530

    added - for tag

--- aws-tasks/ec2-insatnce-start.tf
+++ aws-tasks/ec2-insatnce-start.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "ec2-start" {
     ami = "ami-04db49c0fb2215364"  
     instance_type = "t2.micro"
     tags = {
-      "Name" = "Learning ${count.index}"
+      "Name" = "Learning-${count.index}"
     }
     count = 2
 }
