93927d7cde49e95a164df53b6cd9525c5baf1b8c
manojreddy <ymrdevops@gmail.com>
2021-08-14 06:55:14 +0530

    ec2 instance code in terraform

---
+++ aws-tasks/ec2-insatnce-start.tf
@@ -0,0 +1,18 @@
+terraform {
+    required_providers {
+      aws = {
+          source = "hashicorp/aws"
+          version = "~>3.20"
+      }
+    }
+}
+
+provider "aws" {
+    profile = "default"
+    region = "ap-south-1"  
+}
+
+resource "aws_instance" "ec2-start" {
+    ami = "ami-04db49c0fb2215364"  
+    instance_type = "t2.micro"
+}
