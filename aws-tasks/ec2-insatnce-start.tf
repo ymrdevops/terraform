44f6e9f2d28ab160cc5c3490f45c7f1b16647470
manojreddy <ymrdevops@gmail.com>
2021-08-14 13:12:33 +0530

    added 2 providers in different regions

--- aws-tasks/ec2-insatnce-start.tf
+++ aws-tasks/ec2-insatnce-start.tf
@@ -12,11 +12,16 @@ provider "aws" {
     region = "ap-south-1"  
 }
 
+provider "aws" {
+  profile = "default"
+  region = "ap-east-1"  
+}
+
 resource "aws_instance" "ec2-start" {
     ami = "ami-04db49c0fb2215364"  
     instance_type = "t2.micro"
     tags = {
       "Name" = "Learning-${count.index}"
     }
-    count = 2
+    count = 1
 }
