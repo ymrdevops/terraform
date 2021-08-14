6834befa6a19656ce210437720ab7c18c8a9be75
manojreddy <ymrdevops@gmail.com>
2021-08-14 13:27:50 +0530

    removed ec2 resource block

--- aws-tasks/ec2-insatnce-start.tf
+++ aws-tasks/ec2-insatnce-start.tf
@@ -18,11 +18,3 @@ provider "aws" {
   alias = "aws-ap-east-1"  
 }
 
-resource "aws_instance" "ec2-start" {
-    ami = "ami-04db49c0fb2215364"  
-    instance_type = "t2.micro"
-    tags = {
-      "Name" = "Learning-${count.index}"
-    }
-    count = 1
-}
