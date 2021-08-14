6ecdceec00c1009591d444aee89335640450b108
manojreddy <ymrdevops@gmail.com>
2021-08-14 13:19:37 +0530

    local name change in east vpc file

--- aws-tasks/vpc-ap-east-1.tf
+++ aws-tasks/vpc-ap-east-1.tf
@@ -1,7 +1,7 @@
-resource "aws_vpc" "myvpc" {
+resource "aws_vpc" "myvpceast" {
     cidr_block = "10.0.0.0/16"
     provider = aws.aws-ap-east-1
     tags = {
-      "Name" = "myvpctest"
+      "Name" = "myvpctesteast"
     } 
 }
