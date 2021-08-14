f94be9658fad42b450def499323660e4c4521ce5
manojreddy <ymrdevops@gmail.com>
2021-08-14 07:05:53 +0530

    added tags to ec2 terraform file & count of instances is 2

--- aws-tasks/ec2-insatnce-start.tf
+++ aws-tasks/ec2-insatnce-start.tf
@@ -15,4 +15,8 @@ provider "aws" {
 resource "aws_instance" "ec2-start" {
     ami = "ami-04db49c0fb2215364"  
     instance_type = "t2.micro"
+    tags = {
+      "Name" = "Learning"
+    }
+    count = 2
 }
